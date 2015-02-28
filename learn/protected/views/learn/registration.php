ff6c17cd058fa2bf09b54db7b5c25f1c3fec00ba
mansisce <mansisce@gmail.com>
2015-02-28 23:13:53 +0530

    added registration basic

---
+++ learn/protected/views/learn/registration.php
@@ -0,0 +1,49 @@
+    <header>
+
+
+        <div class="coursetype">
+
+            <div>
+                Classroom training<span></span>
+            </div>
+
+            <div>
+                Online Self Learning
+            </div>
+
+        </div>
+
+    </header>
+    <div class="wrapper">
+
+        <nav>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+
+        <div class="feature">
+            <img src="assets/images/content.jpg" width="550px" height="" />
+            <div class="actionbuy">Buy Now</div>
+            <img src="images/about_course.jpg" width="" height="" />
+        </div>
+
+        <div class="trainer">
+            <div>
+                <img src="css/images/mentor.jpg" width="" height="" />
+            </div>
+            <div>
+                <img src="css/images/trainer.jpg" width="" height="" />
+            </div>
+            <div>
+                <img src="css/images/sr.trainer.jpg" width="" height="" />
+            </div>
+            <div>
+                <img src="css/images/super sr.trainer.jpg" width="" height="" />
+            </div>
+        </div>
+
+    </div>
