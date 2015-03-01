c56f5472f9b688b838b8042c795373d52d9ee2a9
mansisce <mansisce@gmail.com>
2015-03-01 11:08:43 +0530

    fail

---
+++ learn/protected/views/learn/fail.php
@@ -0,0 +1,8 @@
+<div class="myaccount">
+    <div class="alert alert-danger" role="alert">
+        <strong>Hey Vijay</strong>
+        <br>We are sorry that the evaluation did not go through. But no worries, here's a chance to take a re-test Re-test
+    </div>
+    <div>Or Go Back to course materials to brush up</div>
+</div>
+
