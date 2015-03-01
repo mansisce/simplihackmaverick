3dbdc15fc7b53c1cd567491b3b77783cc802eafd
mansisce <mansisce@gmail.com>
2015-03-01 06:51:39 +0530

    addedresult

---
+++ learn/protected/views/learn/result.php
@@ -0,0 +1,7 @@
+<div class="myaccount">
+    <div class="alert alert-success" role="alert">
+        <strong>Congratulations! Vijay</strong><br>You have scored 7 out 10 marks. That’s one among 1421 trainers.
+    </div>
+</div>
+
+
